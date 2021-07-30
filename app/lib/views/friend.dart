ccc3d71f8809e48c0b79eb4f930749d2e63d648b
Bart Vermeulen <bart.vermeulen@student.fontys.nl>
2021-07-30 12:09:13 +0200

    Finished the first version of the friend page

--- app/lib/views/friend.dart
+++ app/lib/views/friend.dart
@@ -1,4 +1,5 @@
 import 'package:assorted_layout_widgets/assorted_layout_widgets.dart';
+import 'package:day/day.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:golfcaddie/components/appbar.dart';
@@ -70,7 +71,65 @@ class _FriendState extends State<FriendScreen> {
                     Padding(
                       padding: EdgeInsets.all(2),
                     ),
-                    ...this.rounds.map((element) => Text(element['startsAt'])).toList(),
+                    ...this
+                        .rounds
+                        .map((e) => Column(
+                              children: [
+                                GestureDetector(
+                                  onTap: () => Navigator.of(context).pushNamed('roundinfo', arguments: e['roundId']),
+                                  child: Container(
+                                    clipBehavior: Clip.hardEdge,
+                                    decoration: BoxDecoration(
+                                        boxShadow: [BoxShadow(color: Colors.black45, offset: Offset.fromDirection(1), blurRadius: 2)],
+                                        color: Theme.of(context).colorScheme.secondary,
+                                        borderRadius: BorderRadius.circular(12)),
+                                    height: 50,
+                                    width: MediaQuery.of(context).size.width * 0.76,
+                                    child: Row(
+                                      children: [
+                                        new Image.network(
+                                          e['image'],
+                                        ),
+                                        Padding(
+                                            padding: EdgeInsets.only(left: 15, right: 15),
+                                            child: SizedBox(
+                                              width: MediaQuery.of(context).size.width * 0.76 - 75,
+                                              child: Row(
+                                                crossAxisAlignment: CrossAxisAlignment.center,
+                                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                                children: [
+                                                  Column(
+                                                    mainAxisAlignment: MainAxisAlignment.center,
+                                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                                    children: [
+                                                      Text(Day.fromString(e['startsAt']).format('DD-MM-YYYY'),
+                                                          style: TextStyle(color: Theme.of(context).colorScheme.onSecondary, fontWeight: FontWeight.bold)),
+                                                      Text(
+                                                        e['name'],
+                                                        style: TextStyle(fontStyle: FontStyle.italic, color: Theme.of(context).colorScheme.onSecondary),
+                                                      )
+                                                    ],
+                                                  ),
+                                                  Spacer(),
+                                                  getIconForPlayers(e)
+                                                ],
+                                              ),
+                                            )),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                Padding(padding: EdgeInsets.all(2))
+                              ],
+                            ))
+                        .toList(),
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                      Icon(Icons.sports_golf_outlined, size: 40),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text(this.friend.handicap.toString(), style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20))
+                    ]),
                     ElevatedButton(
                         style: ElevatedButton.styleFrom(primary: Theme.of(context).colorScheme.error),
                         onPressed: () => removeFriend(),
@@ -90,7 +149,6 @@ class _FriendState extends State<FriendScreen> {
     Dio dio = await AppUtils.getDio();
     dio.get("/friend/get/" + id).then((value) {
       Map<String, dynamic> friend = value.data['friend'];
-      print(friend);
       this.friend = Friend(friend['name'], friend['handicap'], friend['id'], friend['image']);
       this.rounds = friend['rounds'];
       this.callMade = true;
@@ -117,5 +175,26 @@ class _FriendState extends State<FriendScreen> {
             backgroundColor: Theme.of(context).colorScheme.error,
           ));
         });
+  }
+
+  Icon getIconForPlayers(round) {
+    int count = 0;
+    if (round['playerOne'] != null) count = count + 1;
+    if (round['playerTwo'] != null) count = count + 1;
+    if (round['playerThree'] != null) count = count + 1;
+    if (round['playerFour'] != null) count = count + 1;
+
+    switch (count) {
+      case 1:
+        return Icon(Icons.looks_one_outlined, color: Colors.white);
+      case 2:
+        return Icon(Icons.looks_two_outlined, color: Colors.white);
+      case 3:
+        return Icon(Icons.looks_3_outlined, color: Colors.white);
+      case 4:
+        return Icon(Icons.looks_4_outlined, color: Colors.white);
+      default:
+        return Icon(Icons.error);
+    }
   }
 }
